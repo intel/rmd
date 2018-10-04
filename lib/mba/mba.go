16807a2cc4fbb4c54dbdb3608743bd5cfaaa0fcd
Shihta Kuan <shihta.kuan@qct.io>
2018-10-04 09:43:32 +0800

    Remove the global variables mbaStep and mbaMin

--- lib/mba/mba.go
+++ lib/mba/mba.go
@@ -8,31 +8,25 @@ import (
 	"github.com/intel/rmd/lib/proc"
 )
 
-var mbaStep, mbaMin = -1, -1
-
 // GetMbaInfo traverse resctrl MB to get info
 var GetMbaInfo = func() (int, int, error) {
-	if mbaStep == -1 || mbaMin == -1 {
-		step, min := 0, 0
-		dat, err := ioutil.ReadFile(proc.MbaInfoPath + "/bandwidth_gran")
-		if err != nil {
-			return step, min, err
-		}
-		step, err = strconv.Atoi(strings.TrimSpace(string(dat)))
-		if err != nil {
-			return step, min, err
-		}
-
-		dat, err = ioutil.ReadFile(proc.MbaInfoPath + "/min_bandwidth")
-		if err != nil {
-			return step, min, err
-		}
-		min, err = strconv.Atoi(strings.TrimSpace(string(dat)))
-		if err != nil {
-			return step, min, err
-		}
-		mbaStep = step
-		mbaMin = min
+	step, min := 0, 0
+	dat, err := ioutil.ReadFile(proc.MbaInfoPath + "/bandwidth_gran")
+	if err != nil {
+		return step, min, err
 	}
-	return mbaStep, mbaMin, nil
+	step, err = strconv.Atoi(strings.TrimSpace(string(dat)))
+	if err != nil {
+		return step, min, err
+	}
+
+	dat, err = ioutil.ReadFile(proc.MbaInfoPath + "/min_bandwidth")
+	if err != nil {
+		return step, min, err
+	}
+	min, err = strconv.Atoi(strings.TrimSpace(string(dat)))
+	if err != nil {
+		return step, min, err
+	}
+	return step, min, nil
 }
